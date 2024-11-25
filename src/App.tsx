503c8a624d1d4adbc5d9f978f6cd25fc3f1696b6
bocajv <jbvoss10@gmail.com>
2024-11-25 12:40:15 -0500

    Updated UI and Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import "./App.css";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dropdown from "react-bootstrap/Dropdown";
 
 declare global {
   interface Window {
@@ -38,8 +41,38 @@ function App() {
   const [technologies, setTechnologies] = useState<string[]>([]);
   const [cookies, setCookies] = useState<Cookie[]>([]);
   const [serverInfo, setServerInfo] = useState<ServerInfo[]>([]);
+  const [isScanStarted, setIsScanStarted] = useState(false);
+
+  const [scanProtocol, setScanProtocol] = useState(true);
+  const [scanHeaders, setScanHeaders] = useState(true);
+  const [scanCookies, setScanCookies] = useState(true);
+  const [scanTechnologies, setScanTechnologies] = useState(true);
+  const [scanServer, setScanServer] = useState(true);
+
+  const toggleScanOption = (option: string) => {
+    switch (option) {
+      case "protocol":
+        setScanProtocol(!scanProtocol);
+        break;
+      case "headers":
+        setScanHeaders(!scanHeaders);
+        break;
+      case "cookies":
+        setScanCookies(!scanCookies);
+        break;
+      case "technologies":
+        setScanTechnologies(!scanTechnologies);
+        break;
+      case "server":
+        setScanServer(!scanServer);
+        break;
+      default:
+        break;
+    }
+  };
 
   const fetchProtocolAndHeaders = async () => {
+    setIsScanStarted(true);
     try {
       chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
         if (tabs[0]?.url) {
@@ -152,57 +185,105 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>DAWG SCAN</h1>
-      <img src="/icon.png" alt="Logo" />
-      <button onClick={fetchProtocolAndHeaders}>Start Scan!</button>
-      <h3>Protocol Used:</h3>
-      <p>
-        <strong>{protocol}</strong>
-      </p>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <h3>Missing Security Headers:</h3>
-      <ul>
-        {missingHeaders.length === 0 && !error ? (
-          <p>No missing security headers.</p>
-        ) : (
-          missingHeaders.map((header) => <li key={header}>{header}</li>)
+    <body className="ext-body">
+      <div className="App">
+        <h1 className="dawg">DAWG SCAN</h1>
+        <img className="img" src="/icon.png" alt="Logo" />
+        <button className="scan-btn" onClick={fetchProtocolAndHeaders}>
+          Start Scan!
+        </button>
+        {/* DropdownButton with title and children */}
+        <DropdownButton title="Select Scan Options" id="dropdown-basic-button">
+          <Dropdown.Item onClick={() => toggleScanOption("protocol")}>
+            Protocol {scanProtocol ? "✅" : ""}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => toggleScanOption("headers")}>
+            Headers {scanHeaders ? "✅" : ""}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => toggleScanOption("technologies")}>
+            Technologies {scanTechnologies ? "✅" : ""}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => toggleScanOption("server")}>
+            Server {scanServer ? "✅" : ""}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => toggleScanOption("cookies")}>
+            Cookies {scanCookies ? "✅" : ""}
+          </Dropdown.Item>
+        </DropdownButton>
+        {isScanStarted && ( // Only display details if the scan has started
+          <>
+            {scanProtocol && (
+              <>
+                <h3>Protocol Used:</h3>
+                <p>
+                  <strong>{protocol}</strong>
+                </p>
+                {error && <p style={{ color: "red" }}>{error}</p>}
+              </>
+            )}
+            {scanHeaders && (
+              <>
+                <h3 className="h3-head">Missing Security Headers:</h3>
+                <ul>
+                  {missingHeaders.length === 0 && !error ? (
+                    <p>No missing security headers.</p>
+                  ) : (
+                    missingHeaders.map((header) => (
+                      <li key={header}>{header}</li>
+                    ))
+                  )}
+                </ul>
+              </>
+            )}
+            {scanTechnologies && (
+              <>
+                <h3 className="h3-head">Detected Technologies:</h3>
+                <ul>
+                  {technologies.length > 0 ? (
+                    technologies.map((tech) => <li key={tech}>{tech}</li>)
+                  ) : (
+                    <p>No technologies detected.</p>
+                  )}
+                </ul>
+              </>
+            )}
+            {scanServer && (
+              <>
+                <h3 className="h3-head">Detected Server Info:</h3>
+                <ul>
+                  {serverInfo.length > 0 ? (
+                    serverInfo.map((info, index) => (
+                      <li key={index}>
+                        <strong>{info.key}:</strong> {info.value}
+                      </li>
+                    ))
+                  ) : (
+                    <p>No server info detected.</p>
+                  )}
+                </ul>
+              </>
+            )}
+            {scanCookies && (
+              <>
+                <h3 className="h3-head">Detected Cookie Info:</h3>
+                <ul>
+                  {cookies.length > 0 ? (
+                    cookies.map((cookie, index) => (
+                      <li key={index}>
+                        {cookie.name}: Secure={String(cookie.secure)}, HttpOnly=
+                        {String(cookie.httpOnly)}, SameSite={cookie.sameSite}
+                      </li>
+                    ))
+                  ) : (
+                    <p>No cookies found on page.</p>
+                  )}
+                </ul>
+              </>
+            )}
+          </>
         )}
-      </ul>
-      <h3>Detected Technologies:</h3>
-      <ul>
-        {technologies.length > 0 ? (
-          technologies.map((tech) => <li key={tech}>{tech}</li>)
-        ) : (
-          <p>No technologies detected.</p>
-        )}
-      </ul>
-      <h3>Detected Server Info:</h3>
-      <ul>
-        {serverInfo.length > 0 ? (
-          serverInfo.map((info, index) => (
-            <li key={index}>
-              <strong>{info.key}:</strong> {info.value}
-            </li>
-          ))
-        ) : (
-          <p>No server info detected.</p>
-        )}
-      </ul>
-      <h3>Detected Cookie Info:</h3>
-      <ul>
-        {cookies.length > 0 ? (
-          cookies.map((cookie, index) => (
-            <li key={index}>
-              {cookie.name}: Secure={String(cookie.secure)}, HttpOnly=
-              {String(cookie.httpOnly)}, SameSite={cookie.sameSite}
-            </li>
-          ))
-        ) : (
-          <p>No cookies found on page.</p>
-        )}
-      </ul>
-    </div>
+      </div>
+    </body>
   );
 }
 
